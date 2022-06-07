c53e965a8840f5c75a66e2ea7ae527a3a0f009e1
nicolaspalacios-f <nicolas.palacios-f@mail.escuelaing.edu.co>
2022-06-07 11:45:26 -0500

    commit definitivo

---
+++ src/main/java/edu/escuelaing/arsw/ASE/app/App.java
@@ -0,0 +1,11 @@
+package edu.escuelaing.arsw.ASE.app;
+
+/**
+ * Hello world!!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
